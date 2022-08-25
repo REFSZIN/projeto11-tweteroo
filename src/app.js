0027bea84db31b801a3e4fdb96ddede3d32b7639
refszin <refwire@gmail.com>
2022-08-24 21:37:36 -0300

    Nodemon in -D

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-import express  from "express";
+import express from "express";
 import cors from "cors";
 
 const app = express();
 app.use(cors());
 
-app.listen(5000, () => {console.log("Rodando Lisu") });
+app.listen(5000, () => {console.log("Rodando Lisu")});
